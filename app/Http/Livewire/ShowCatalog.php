394f75b95cf48b60af609eeb5157188b7335bd82
Paul <6256859+paul1522@users.noreply.github.com>
2023-06-08 22:26:30 -0500

    Improved catalog page.

--- app/Http/Livewire/ShowCatalog.php
+++ app/Http/Livewire/ShowCatalog.php
@@ -3,13 +3,10 @@
 namespace App\Http\Livewire;
 
 use App\Models\ItemLocation;
-use Closure;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Tables;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class ShowCatalog extends Component implements HasTable
@@ -31,37 +28,34 @@ class ShowCatalog extends Component implements HasTable
          * and `location_user`.`user_id` = ?
          */
         return ItemLocation::query()
-            ->where('quantity', '>', 0)
+            // ->where('quantity', '>', 0)
             ->whereRaw('location_id in (select location_id from location_user where user_id = '.request()->user()->id.')');
     }
 
     protected function getTableColumns(): array
     {
         return [
-            TextColumn::make('location.name')->label('Location')->sortable()->searchable(),
-            TextColumn::make('item.group')->label('Group')->sortable()->searchable(),
-            TextColumn::make('item.manufacturer')->label('Manufacturer')->sortable()->searchable(),
-            TextColumn::make('item.description')->label('Description')->sortable()->searchable(),
-            TextColumn::make('item.key')->label('Key')->sortable()->searchable(),
-            TextColumn::make('item.supplier_key')->label('Supplier Key')->sortable()->searchable(),
-            TextColumn::make('quantity')->sortable(),
+            Tables\Columns\TextColumn::make('location.name')->label('Location')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('item.group')->label('Group')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('item.manufacturer')->label('Manufacturer')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('item.description')->label('Description')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('item.key')->label('Key')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('item.supplier_key')->label('Supplier Key')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('quantity')->sortable(),
         ];
     }
 
-//    protected function paginateTableQuery(Builder $query): Paginator
-//    {
-//        $q =  $query->simplePaginate($this->getTableRecordsPerPage() == -1 ? $query->count() : $this->getTableRecordsPerPage());
-//        if ($this->page === 2) $q->dd();
-//        return $q;
-//    }
-
-//    protected function getDefaultTableSortColumn(): ?string
-//    {
-//        return 'item.key';
-//    }
-
-//    protected function getTableRecordUrlUsing(): ?Closure
-//    {
-//        return fn (ItemLocation $record): string => route('product', ['product' => $record]);
-//    }
+    protected function getTableFilters(): array
+    {
+        return [
+            Tables\Filters\Filter::make('exclude_out_of_stock_items')
+                ->query(fn (Builder $query): Builder => $query->where('quantity', '>', 0))
+                ->default(),
+            Tables\Filters\Filter::make('exclude_parts_and_accessories')
+                ->query(fn (Builder $query): Builder => $query->whereRaw('item_id not in (select item_id from bill_of_materials)'))
+                ->default(),
+            Tables\Filters\SelectFilter::make('location')
+                ->relationship('location', 'name'),
+        ];
+    }
 }
