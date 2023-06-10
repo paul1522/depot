b6aa335a47ba76b35ca335c1e35e6915f83e70d4
Paul <6256859+paul1522@users.noreply.github.com>
2023-06-10 10:29:24 -0500

    WIP on shopping cart.

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -7,5 +7,10 @@
 
     <x-page.content>
         {{ $this->table }}
+
+        <div class="my-2">
+            <a href="{{route('catalog.show')}}" class="dui-btn dui-btn-neutral">{{ __('Return to Catalog') }}</a>
+            <a href="{{route('checkout')}}" class="dui-btn dui-btn-neutral">{{ __('Checkout') }}</a>
+        </div>
     </x-page.content>
 </div>
