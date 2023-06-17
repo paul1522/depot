8726dca77681c5cfc3888c737658886c33f851b7
Paul <6256859+paul1522@users.noreply.github.com>
2023-06-17 00:23:12 -0500

    Added some filter options to the catalog page.

--- app/Http/Livewire/ShowCatalog.php
+++ app/Http/Livewire/ShowCatalog.php
@@ -4,9 +4,11 @@ namespace App\Http\Livewire;
 
 use App\Models\Item;
 use App\Models\ItemLocation;
+use App\Models\Location;
 use Closure;
 use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class ShowCatalog extends Component implements Tables\Contracts\HasTable
@@ -20,24 +22,27 @@ class ShowCatalog extends Component implements Tables\Contracts\HasTable
 
     protected function getTableQuery(): Builder
     {
-        /*
-         * select *
-         * from `item_locations`
-         * inner join `location_user` on `location_user`.`location_id` = `item_locations`.`location_id`
-         * where `quantity` > ?
-         * and `location_user`.`user_id` = ?
-         */
         return ItemLocation::query()
-            // ->where('quantity', '>', 0)
-            ->whereRaw('location_id in (select location_id from location_user where user_id = '.request()->user()->id.')');
+            ->selectRaw('item_locations.id, item_locations.location_id, item_locations.item_id, item_locations.quantity, items.`group` as `group`, items.manufacturer as manufacturer')
+            ->join('items', 'items.id', '=', 'item_locations.item_id')
+            ->whereIn('location_id', $this->locationIds());
+    }
+
+    protected function locationIds(): array
+    {
+        return DB::table('location_user')
+            ->select('location_id')
+            ->where('user_id', '=', request()->user()->id)
+            ->pluck('location_id')
+            ->toArray();
     }
 
     protected function getTableColumns(): array
     {
         return [
             Tables\Columns\TextColumn::make('location.name')->label('Location')->sortable()->searchable(),
-            Tables\Columns\TextColumn::make('item.group')->label('Group')->sortable()->searchable(),
-            Tables\Columns\TextColumn::make('item.manufacturer')->label('Manufacturer')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('group')->label('Group')->sortable()->searchable(),
+            Tables\Columns\TextColumn::make('manufacturer')->label('Manufacturer')->sortable()->searchable(),
             Tables\Columns\TextColumn::make('item.description')->label('Description')->sortable()->searchable(),
             Tables\Columns\TextColumn::make('item.key')->label('Key')->sortable()->searchable(),
             Tables\Columns\TextColumn::make('item.supplier_key')->label('Supplier Key')->sortable()->searchable(),
@@ -54,13 +59,45 @@ class ShowCatalog extends Component implements Tables\Contracts\HasTable
             Tables\Filters\Filter::make('exclude_parts_and_accessories')
                 ->query(fn (Builder $query): Builder => $query->whereRaw('item_id not in (select item_id from bill_of_materials)'))
                 ->default(),
-            Tables\Filters\SelectFilter::make('location')
-                ->relationship('location', 'name'),
+            Tables\Filters\SelectFilter::make('location')->options($this->locationOptions())->attribute('location_id'),
+            Tables\Filters\SelectFilter::make('group')->options($this->groupOptions()),
+            Tables\Filters\SelectFilter::make('manufacturer')->options($this->manufacturerOptions()),
         ];
     }
 
     protected function getTableRecordUrlUsing(): ?Closure
     {
         return fn (ItemLocation $itemLocation): string => route('item.show', ['id' => $itemLocation->id]);
+    }
+
+    private function locationOptions(): array
+    {
+        return Location::whereIn('id', $this->locationIds())->pluck('name', 'id')->toArray();
+    }
+
+    private function groupOptions(): array
+    {
+        return Item::distinct()
+            ->whereIn('id', $this->itemIds())
+            ->pluck('group', 'group')
+            ->toArray();
+    }
+
+    private function manufacturerOptions(): array
+    {
+        return Item::distinct()
+            ->whereIn('id', $this->itemIds())
+            ->pluck('manufacturer', 'manufacturer')
+            ->toArray();
+    }
+
+    private function itemIds(): array
+    {
+        return DB::table('item_locations')
+            ->distinct()
+            ->select('item_id')
+            ->whereIn('location_id', $this->locationIds())
+            ->pluck('item_id')
+            ->toArray();
     }
 }
